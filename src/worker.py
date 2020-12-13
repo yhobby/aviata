f02dbcb1de00c9d032d3c2305dbd49675dd28333
yhobby <nissan_kairat@mail.ru>
2020-12-14 01:26:35 +0600

    changed run time

--- src/worker.py
+++ src/worker.py
@@ -8,22 +8,25 @@ from src.api import search_flights, check_flights
 from src.settings import IATA_CODES
 
 db = redis.Redis(host='redis')
+# db = redis.Redis()
 app = Celery(broker='redis://redis:6379/1')
 
 app.conf.timezone = 'Asia/Almaty'
+# The function run time is set not correct only for verification the task
+# Correct time crontab(minute=0, hour=0)
 app.conf.beat_schedule = {
     'midnight_updates': {
         'task': 'src.worker.midnight_updates',
-        'schedule': crontab(minute=0, hour=0)
+        'schedule': crontab() # Execute every minutes
     },
     'price_confirmations': {
         'task': 'src.worker.price_confirmations',
-        'schedule': crontab(minute='*/1')
+        'schedule': crontab(minute='*/5') # Execute every 5 minutes
     },
 }
 
 
-# Runs every day at 00:00
+# Execute daily at midnight.
 @app.task()
 def midnight_updates():
     routes = generate_unique_routes()
@@ -34,7 +37,7 @@ def midnight_updates():
             db.hset(key, None, None, value)
 
 
-# Starts every 1 minute
+# Execute every minutes.
 @app.task()
 def price_confirmations():
     routes = generate_unique_routes()
